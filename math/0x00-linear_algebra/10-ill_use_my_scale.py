5fcccd2d0a24ed894080a48b79c25a2b10342b70
Jose Andres Fabre <98056093+Joanfa7@users.noreply.github.com>
2023-03-20 11:13:22 -0400

    Delete math/0x00-linear_algebra directory

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" function than calculates the shape of a numpy.ndarray"""
-
-
-def np_shape(matrix):
-    """ using numpy to calculate the shape of a matrix """
-    return matrix.shape
--- math/0x00-linear_algebra/3-flip_me_over.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Function that returns transpose of a 2D matrix"""
-
-
-def matrix_transpose(matrix):
-    """ Function that transpose a 2D matrix"""
-    if not matrix:
-        return []
-    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
