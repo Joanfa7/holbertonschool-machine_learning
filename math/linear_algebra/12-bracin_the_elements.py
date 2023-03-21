6891943bdc48ce9d9437ed7b97c818d69c94cd7b
joanfa7 <jafabre95@gmail.com>
2023-03-21 12:59:04 -0400

    adding new file

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,13 +1,10 @@
 #!/env/bin/env python3
 """ performs elemetn-wise addition, subtraciton, multiplication, and division"""
-
+import numpy as np
 
 def np_elementwise(mat1, mat2):
-    sum_matrices = mat1 + mat2
-    sub_matrices = mat1 - mat2
-    mult_matrices = mat1 * mat2
-    div_matrices = mat1 / mat2
-
-    return(sum_matrices, sub_matrices, mult_matrices, div_matrices)
-
-
+    add = np.add(mat1, mat2)
+    sub = np.subtract(mat1, mat2)
+    mul = np.multiply(mat1, mat2)
+    div = np.divide(mat1, mat2)
+    return add, sub, mul, div
