43f4c87a24f8288fd79f8adb38ec9f2be382a34f
joanfa7 <jafabre95@gmail.com>
2023-03-21 14:42:26 -0400

    adding new file

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""  function def np_matmul(mat1, mat2): that performs matrix multiplication:"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Multyply matrix"""
+    return np.matmul(mat1, mat2)
