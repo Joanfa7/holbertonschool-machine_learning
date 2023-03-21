a836ea39e4bad294a20f54e14958db47532102be
joanfa7 <jafabre95@gmail.com>
2023-03-21 18:41:26 -0400

    adding new file

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -2,6 +2,11 @@
 """ funciton that concatenates 2 matrices """
 
 
-def matrices2D(mat1, mat2, axis=0):
-    new_matrix = []
-    
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ Function that concatenates a matrix in specific axis."""
+    if axis == 0:
+        return mat1 + mat2
+    elif axis == 1:
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+    else:
+        return None
