b07512207f098fb52e348a5e7dcceffb60e65757
joanfa7 <jafabre95@gmail.com>
2023-03-29 17:16:38 -0400

    adding double file

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -7,6 +7,8 @@ def poly_integral(poly, C=0):
     """Function that calculates de integral of a polynomial"""
     if not isinstance(poly, list) or not isinstance(C, int):
         return None
+    elif len(poly) == 1:
+        return [0]
     elif len(poly) == 0:
         return None
     else:
