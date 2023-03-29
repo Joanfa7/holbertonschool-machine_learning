a064e7b3107e01fa80435b4650bbbd96bc69293b
joanfa7 <jafabre95@gmail.com>
2023-03-29 17:20:14 -0400

    adding double file

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -7,10 +7,10 @@ def poly_integral(poly, C=0):
     """Function that calculates de integral of a polynomial"""
     if not isinstance(poly, list) or not isinstance(C, int):
         return None
+    elif len(poly) == 0:
+        return [0] 
     elif len(poly) == 1:
         return [C, poly[0]]
-    elif len(poly) == 0:
-        return [0]
     else:
         integral = [C] + [poly[i] / (i + 1) for i in range(len(poly))]
         rounded_integral = [round(i, 2) for i in integral]
