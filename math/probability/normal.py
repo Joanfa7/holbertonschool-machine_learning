ffe00d904d04246b899cfe8e3b775606121041f0
joanfa7 <jafabre95@gmail.com>
2023-04-05 15:38:15 -0400

    Adding to Normal class z_scrore and x_value method

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -4,6 +4,7 @@
 
 class Normal:
     '''Create a normal distribution'''
+
     def __init__(self, data=None, mean=0., stddev=1.):
         '''Class constructor'''
         if data is None:
@@ -19,3 +20,11 @@ class Normal:
             self.mean = sum(data) / len(data)
             self.stddev = (sum([(x - self.mean) ** 2 for x in data]) /
                            len(data)) ** 0.5
+
+    def z_score(self, x):
+        '''Calculates the z-score of a given x-value'''
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        '''Calculates the x-value of a given z-score'''
+        return z * self.stddev + self.mean
