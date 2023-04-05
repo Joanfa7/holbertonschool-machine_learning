ec5f9c63a2f0b5d00d3dd36081f806d4c0d9d4f1
joanfa7 <jafabre95@gmail.com>
2023-04-05 09:39:08 -0400

    Adding pmf to posson class

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -20,3 +20,15 @@ class Poisson:
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """ Calculates the value of the PMF for a given number of successes
+            k: number of successes
+        """
+        if k < 0:
+            return 0
+        k = int(k)
+        fact = 1
+        for i in range(1, k + 1):
+            fact *= i
+        return (self.lambtha ** k * 2.7182818285 ** (-self.lambtha)) / fact
