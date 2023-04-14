490bedea8e5a1d692e1a67ad820c6c4178b640d0
joanfa7 <jafabre95@gmail.com>
2023-04-14 19:51:23 -0400

    checking

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -15,26 +15,26 @@ class Neuron:
                     b (int): The bias for the neuron.
                     A (int): The activated output of the neuron (prediction).
         """
-        if not isinstance(nx, int):
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.__W = np.random.randn(nx).reshape(1, nx)
-        self.__b = 0
-        self.__A = 0
+        self._W = np.random.randn(nx).reshape(1, nx)
+        self._b = 0
+        self._A = 0
 
         @property
         def W(self):
             """ getter for W """
-            return self.__W
+            return self._W
 
         @property
         def b(self):
             """ getter for b """
-            return self.__b
+            return self._b
 
         @property
         def A(self):
             """ getter for A """
-            return self.__A
+            return self._A
