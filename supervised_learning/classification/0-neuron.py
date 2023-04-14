2c57a62423560b33ea8de43bf85e2165d369da45
joanfa7 <jafabre95@gmail.com>
2023-04-14 18:05:19 -0400

    Fixing code, adding data dir

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -3,14 +3,16 @@
 
 import numpy as np
 
+
 class Neuron:
-    """ Neuron class.""" 
+    """ Neuron class."""
+
     def __init__(self, nx):
         """ Class constructor. """
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.__W = np.random.normal(0, 1, (1, nx))
-        self.__b = 0
-        self.__A = 0
+        self.W = np.random.normal(0, 1, (1, nx))
+        self.b = 0
+        self.A = 0
