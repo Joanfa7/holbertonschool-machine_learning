b22923e95a50d83dcf3d44abfcce811229b9f901
joanfa7 <jafabre95@gmail.com>
2023-04-15 12:47:46 -0400

    fix pycodestyle

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -2,25 +2,27 @@
 """ Deep Neural Network """
 import numpy as np
 
+
 class DeepNeuralNetwork:
     """ Class DeepNeuralNetwork """
 
     def __init__(self, nx, layers):
         """ Class constructor """
-        if isinstance(layers, int): # Check if layers is an integer
-            layers = [layers] # Convert the integer to a list with a single element
-        
+        if isinstance(layers, int):  # Check if layers is an integer
+            # Convert the integer to a list with a single element
+            layers = [layers]
+
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        if type(nx) is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or len(layers) == 0:
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
         for i in range(len(layers)):
-            if type(layers[i]) is not int or layers[i] < 1:
+            if not isinstance(layers[i], int) or layers[i] < 1:
                 raise TypeError("layers must be a list of positive integers")
             self.weights["W{}".format(i + 1)] = np.random.randn(
                 layers[i], nx) * np.sqrt(2 / nx)
