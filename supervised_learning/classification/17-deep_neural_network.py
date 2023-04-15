ad0a5776946a149bc6144dc86a0ce4376cb6695b
joanfa7 <jafabre95@gmail.com>
2023-04-15 12:51:35 -0400

    adding getters

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+""" Deep Neural Network """
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class DeepNeuralNetwork """
+
+    def __init__(self, nx, layers):
+        """ Class constructor """
+        if isinstance(layers, int):  # Check if layers is an integer
+            # Convert the integer to a list with a single element
+            layers = [layers]
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        for i in range(len(layers)):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights["W{}".format(i + 1)] = np.random.randn(
+                layers[i], nx) * np.sqrt(2 / nx)
+            self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+            nx = layers[i]
+
+    @property
+    def L(self):
+        """ Getter for L """
+        return self.__L
+    
+    @property
+    def cache(self):
+        """ Getter for cache """
+        return self.__cache
+    
+    @property
+    def weights(self):
+        """ Getter for weights """
+        return self.__weights
+    
+    
