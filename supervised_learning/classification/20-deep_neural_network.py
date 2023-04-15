fe9e757dc57878a6599fc6519cc362111398f833
joanfa7 <jafabre95@gmail.com>
2023-04-15 13:14:24 -0400

    adding evaluate function

---
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+""" Deep Neural Network """
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class DeepNeuralNetwork """
+
+    def __init__(self, nx, layers):
+        """ Class constructor """
+        if isinstance(layers, int):
+            layers = [layers]
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        for i in range(len(layers)):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            self.__weights["W{}".format(i + 1)] = np.random.randn(
+                layers[i], nx) * np.sqrt(2 / nx)
+            self.__weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
+            nx = layers[i]
+
+    @property
+    def L(self):
+        """ Getter method for L """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Getter method for cache """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ Getter method for weights """
+        return self.__weights
+
+    def forward_prop(self, X):
+        """ Calculates the forward propagation of the neural network """
+        self.__cache["A0"] = X
+        for i in range(self.__L):
+            z = np.matmul(self.__weights["W{}".format(i + 1)],
+                          self.__cache["A{}".format(i)]) + \
+                self.__weights["b{}".format(i + 1)]
+            self.__cache["A{}".format(i + 1)] = 1 / (1 + np.exp(-z))
+        return self.__cache["A{}".format(i + 1)], self.__cache
+
+    def cost(self, Y, A):
+        """ Calculates the cost of the model using logistic regression """
+        m = Y.shape[1]
+        cost = -1 / m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neural network’s predictions """
+        A, _ = self.forward_prop(X)
+        return np.where(A >= 0.5, 1, 0), self.cost(Y, A)
