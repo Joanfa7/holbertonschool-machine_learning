2ab534e8ece4b05ac9efdc258e33f075d165e36f
joanfa7 <jafabre95@gmail.com>
2023-04-15 12:38:24 -0400

    fix bug

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -8,24 +8,20 @@ class DeepNeuralNetwork:
     """ Class DeepNeuralNetwork """
 
     def __init__(self, nx, layers):
-        """ Constructor """
+        """ Class constructor """
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        if not isinstance(nx, int):
+        if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list):
-            raise TypeError("layers must be a list of positive integers")
-        if len(layers) == 0:
+        if type(layers) is not list or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
         for i in range(len(layers)):
-            print("i = {}".format(i))
-            print("layers[i] = {}".format(layers[i]))
-            if not isinstance(layers[i], int) or layers[i] < 1:
+            if type(layers[i]) is not int or layers[i] < 1:
                 raise TypeError("layers must be a list of positive integers")
-            self.weights["W{}".format(
-                i + 1)] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+            self.weights["W{}".format(i + 1)] = np.random.randn(
+                layers[i], nx) * np.sqrt(2 / nx)
             self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
             nx = layers[i]
