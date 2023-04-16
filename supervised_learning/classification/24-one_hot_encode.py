1b64f759ed7bbbfc9867571755ca6fd6789fb83e
joanfa7 <jafabre95@gmail.com>
2023-04-16 14:16:07 -0400

    fix pycodestyle

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -3,11 +3,12 @@
 
 import numpy as np
 
+
 def one_hot_encode(Y, classes):
     """ One Hot Encode """
-    if type(Y) is not np.ndarray or len(Y) == 0:
+    if not isinstance(Y, np.ndarray) or len(Y) == 0:
         return None
-    if type(classes) is not int or classes <= np.amax(Y):
+    if not isinstance(classes, int) or classes <= np.amax(Y):
         return None
     one_hot = np.zeros((classes, len(Y)))
     one_hot[Y, np.arange(len(Y))] = 1
