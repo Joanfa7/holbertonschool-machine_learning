07e91fe65bc6226b45c0e505fc4a44bb33995ab4
joanfa7 <jafabre95@gmail.com>
2023-04-16 13:58:48 -0400

    fixing bug

--- supervised_learning/classification/22-deep_neural_network.py
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -76,17 +76,17 @@ class DeepNeuralNetwork:
             self.__weights["W{}".format(i)] -= alpha * dw
             self.__weights["b{}".format(i)] -= alpha * db
 
-        def train(self, X, Y, iterations=5000, alpha=0.05):
-            """ Trains the deep neural network """
-            if not isinstance(iterations, int):
-                raise TypeError("iterations must be an integer")
-            if iterations < 0:
-                raise ValueError("iterations must be a positive integer")
-            if not isinstance(alpha, float):
-                raise TypeError("alpha must be a float")
-            if alpha < 0:
-                raise ValueError("alpha must be positive")
-            for i in range(iterations):
-                A, cache = self.forward_prop(X)
-                self.gradient_descent(Y, cache, alpha)
-            return self.evaluate(X, Y)
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ Trains the deep neural network """
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+        for i in range(iterations):
+            A, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
+        return self.evaluate(X, Y)
