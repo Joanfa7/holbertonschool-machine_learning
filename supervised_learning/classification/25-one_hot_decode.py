590083667fdf922e06b10dc96fa33ee075623aae
joanfa7 <jafabre95@gmail.com>
2023-04-16 14:21:56 -0400

    fix bug

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -6,6 +6,6 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ One Hot Decode """
-    if not isinstance(one_hot, np.ndarray) or len(one_hot) == 2:
+    if not isinstance(one_hot, np.ndarray) and len(one_hot) == 2:
         return np.argmax(one_hot, axis=0)
     return None
