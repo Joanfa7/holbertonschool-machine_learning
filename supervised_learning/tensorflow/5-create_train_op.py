3793f7d73f61b82ce867979f4bd4c028ea228da3
joanfa7 <jafabre95@gmail.com>
2023-04-21 13:03:55 -0400

    fix pycode style

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -4,7 +4,8 @@
 import tensorflow as tf
 
 def create_train_op(loss, alpha):
-    """Function that creates the training operation for the network"""
+    """Function that creates the training operation 
+    for the network"""
     optimizer = tf.train.GradientDescentOptimizer(alpha)
     train = optimizer.minimize(loss)
     return train
