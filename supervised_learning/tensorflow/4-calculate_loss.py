75e0893c3bc77b34342ad1b6c5acf382a987c519
joanfa7 <jafabre95@gmail.com>
2023-04-21 12:59:52 -0400

    fix pycode syle

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 
 def calculate_loss(y, y_pred):
-    """Function that calculates the softmax cross-entropy loss of a prediction"""
+    """Function that calculates the softmax cross-entropy loss of a
+    prediction"""
     loss = tf.losses.softmax_cross_entropy(y, y_pred)
     return loss
