6e46a12c87b302e65979dda2dff99d6d324dfd8f
joanfa7 <jafabre95@gmail.com>
2023-04-21 12:55:59 -0400

    fix pycodestyle

--- supervised_learning/tensorflow/3-calculate_accuracy.py
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -3,6 +3,7 @@
 
 import tensorflow as tf
 
+
 def calculate_accuracy(y, y_pred):
     """Function that calculates the accuracy of a prediction"""
     prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
