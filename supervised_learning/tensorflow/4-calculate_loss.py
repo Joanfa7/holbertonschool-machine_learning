4213f4f9e01d7f9f22303a4c588c345219296f81
joanfa7 <jafabre95@gmail.com>
2023-04-21 12:58:37 -0400

    adding new file

---
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+''' Forward Propagation'''
+
+import tensorflow as tf
+
+
+def calculate_loss(y, y_pred):
+    """Function that calculates the softmax cross-entropy loss of a prediction"""
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
+    return loss
