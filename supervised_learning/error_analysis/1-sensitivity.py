8f4f32498fb081b75aa2d7b0f798c1d1ea3ab85e
joanfa7 <jafabre95@gmail.com>
2023-05-02 12:05:36 -0400

    adding sensitive funciton

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Sensitivity """
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """ calculates the sensitivity for each class in a confusion matrix
+        confusion is a confusion numpy.ndarray of shape (classes, classes)
+            where row indices represent the correct labels and column indices
+            represent the predicted labels
+            classes is the number of classes
+        Returns: a numpy.ndarray of shape (classes,) containing the sensitivity
+            of each class
+    """
+    return np.diagonal(confusion) / np.sum(confusion, axis=1)
