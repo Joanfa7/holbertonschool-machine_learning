8ff156bed005ccc0ee97a8d0050af90b8f0b494c
joanfa7 <jafabre95@gmail.com>
2023-05-02 12:14:31 -0400

    fix pycodestyle

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 def precision(confusion):
     """calculates the precision for each class in a confusion matrix"""
     return np.diagonal(confusion) / np.sum(confusion, axis=0)
