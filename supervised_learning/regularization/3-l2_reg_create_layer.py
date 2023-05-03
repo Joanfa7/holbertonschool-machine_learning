9c1289b2895e2c4fa880d6eafc960fbeab477b02
joanfa7 <jafabre95@gmail.com>
2023-05-03 14:44:38 -0400

    adding l2 reg create layer function

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,7 +5,8 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Function that creates a tensorflow layer that includes L2 regularization"""
+    """Function that creates a tensorflow layer that includes L2 
+    regularization"""
     kernel = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     l2 = tf.contrib.layers.l2_regularizer(lambtha)
     layer = tf.layers.Dense(units=n, activation=activation,
