689ca6c2d3ce9b460f13b5b2350a624b57ab6b97
joanfa7 <jafabre95@gmail.com>
2023-05-03 16:23:23 -0400

    adding early stop function

---
+++ supervised_learning/regularization/7-early_stopping.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Early Stopping"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """Determines if you should stop gradient descent early"""
+    if opt_cost - cost > threshold:
+        count = 0
+    else:
+        count += 1
+    if count == patience:
+        return True, count
+    else:
+        return False, count
