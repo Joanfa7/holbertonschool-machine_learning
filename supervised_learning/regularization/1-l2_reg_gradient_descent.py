7bbe45f8ca901325182caa8c3f8416b780616c1c
joanfa7 <jafabre95@gmail.com>
2023-05-03 14:19:38 -0400

    adding l2 function

---
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """updates the weights and biases of a neural network using gradient descent with L2 regularization"""
+    m = Y.shape[1]
+    dZ = cache['A' + str(L)] - Y
+    for i in range(L, 0, -1):
+        A = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+        dW = (1 / m) * np.matmul(dZ, A.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        dZ = np.matmul(W.T, dZ) * (1 - A ** 2)
+        weights['W' + str(i)] = weights['W' + str(i)] - alpha * dW
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
