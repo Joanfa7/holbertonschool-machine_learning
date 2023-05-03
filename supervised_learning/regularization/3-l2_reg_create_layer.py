cd39fd027d2fdcd7e0d2b2aa4867b73ef3e348a5
joanfa7 <jafabre95@gmail.com>
2023-05-03 16:51:50 -0400

    fixing pycodestyle

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Function that creates a tensorflow layer that includes L2 
+    """Function that creates a tensorflow layer that includes L2
     regularization"""
     kernel = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     l2 = tf.contrib.layers.l2_regularizer(lambtha)
