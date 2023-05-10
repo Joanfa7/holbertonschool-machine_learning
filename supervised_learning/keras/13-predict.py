f91c51cc372199024b2bb334f0fa449d5a015e56
joanfa7 <jafabre95@gmail.com>
2023-05-10 18:22:22 -0400

    predict function

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Function that makes a prediction using a neural network '''
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    ''' Function that makes a prediction using a neural network '''
+    return network.predict(data, verbose=verbose)
