ee77d129e1953ec4606d3be9a1d53d9a72740611
joanfa7 <jafabre95@gmail.com>
2023-05-10 14:12:58 -0400

    fix pycodestyle

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -4,8 +4,15 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None,
-                verbose=True, shuffle=False):
+def train_model(
+        network,
+        data,
+        labels,
+        batch_size,
+        epochs,
+        validation_data=None,
+        verbose=True,
+        shuffle=False):
     ''' Function that trains a model using mini-batch gradient descent '''
     return network.fit(data, labels, batch_size=batch_size, epochs=epochs,
                        verbose=verbose, shuffle=shuffle,
