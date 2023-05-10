d6856cefcb557662f3267d85b2b70764af13d6d0
joanfa7 <jafabre95@gmail.com>
2023-05-10 14:33:09 -0400

    adding new file

---
+++ supervised_learning/keras/8-train.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+''' Function that trains a model using mini-batch gradient descent '''
+
+import tensorflow.keras as K
+
+
+def train_model(
+        network,
+        data,
+        labels,
+        batch_zize,
+        epochs,
+        validation_data=None,
+        early_stopping=False,
+        patience=0,
+        learning_rate_decay=False,
+        alpha=0.1,
+        decay_rate=1,
+        save_best=False,
+        filepath=None,
+        verbose=True,
+        shuffle=False):
+    ''' Function that trains a model using mini-batch gradient descent '''
+    callbacks = []
+    if validation_data:
+        if early_stopping:
+            callbacks.append(K.callbacks.EarlyStopping(patience=patience))
+        if learning_rate_decay:
+            def scheduler(epoch):
+                ''' Scheduler function '''
+                return alpha / (1 + decay_rate * epoch)
+            callbacks.append(K.callbacks.LearningRateScheduler(scheduler,
+                                                               verbose=1))
+        if save_best:
+            callbacks.append(K.callbacks.ModelCheckpoint(filepath,
+                                                         save_best_only=True))
+    return network.fit(data, labels, batch_size=batch_zize, epochs=epochs,
+                       verbose=verbose, shuffle=shuffle,
+                       validation_data=validation_data,
+                       callbacks=callbacks)
