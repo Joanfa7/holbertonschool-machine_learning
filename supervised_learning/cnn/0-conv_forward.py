086651ad596b6f6ffbafac9b72d44519c23276e2
joanfa7 <jafabre95@gmail.com>
2023-05-21 21:18:41 -0400

    fix pycodestyel

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     ''' performs forward propagation over a pooling layer of a neural network
         A_prev ndarray (m, h_prev, w_prev, c_prev) input to pool layer
@@ -28,10 +29,9 @@ def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
     for i in range(h_out):
         for j in range(w_out):
             if mode == 'max':
-                A[:, i, j, :] = np.max(A_prev[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :],
-                                       axis=(1, 2))
+                A[:, i, j, :] = np.max(
+                    A_prev[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :], axis=(1, 2))
             if mode == 'avg':
-                A[:, i, j, :] = np.mean(A_prev[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :],
-                                        axis=(1, 2))
+                A[:, i, j, :] = np.mean(
+                    A_prev[:, i * sh:i * sh + kh, j * sw:j * sw + kw, :], axis=(1, 2))
     return A
-
