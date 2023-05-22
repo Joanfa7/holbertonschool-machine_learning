3ea68c7a3c23b7f45ffd68462b176d4de737cc39
joanfa7 <jafabre95@gmail.com>
2023-05-21 22:14:54 -0400

    adding new file

---
+++ supervised_learning/cnn/4-lenet5.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+''' modified LeNet-5 using tensorflow '''
+
+import tensorflow.compat.v1 as tf
+
+
+def lenet5(x, y):
+    ''' builds a modified version of the LeNet-5 architecture using tensorflow
+        x: tf.placeholder (m, 28, 28, 1) input images
+        y: tf.placeholder (m, 10) one-hot labels
+        m: number of images
+        Returns: a tensor for the softmax activated output
+                 a training operation that utilizes Adam optimization
+                 a tensor for the loss of the network
+                 a tensor for the accuracy of the network
+    '''
+    init = tf.contrib.layers.variance_scaling_initializer(scale=2.0)
+    conv1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
+                             padding='same', activation=tf.nn.relu,
+                             kernel_initializer=init)(x)
+    pool1 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv1)
+    conv2 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                             padding='valid', activation=tf.nn.relu,
+                             kernel_initializer=init)(pool1)
+    pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv2)
+    pool2 = tf.layers.Flatten()(pool2)
+    fc1 = tf.layers.Dense(units=120, activation=tf.nn.relu,
+                          kernel_initializer=init)(pool2)
+    fc2 = tf.layers.Dense(units=84, activation=tf.nn.relu,
+                          kernel_initializer=init)(fc1)
+    fc3 = tf.layers.Dense(units=10, kernel_initializer=init)(fc2)
+    y_pred = tf.nn.softmax(fc3)
+    loss = tf.losses.softmax_cross_entropy(y, fc3)
+    train_op = tf.train.AdamOptimizer().minimize(loss)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(fc3, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return y_pred, train_op, loss, accuracy
