177fd07b0e578171cdf0e3f1248b3c64d343ff23
joanfa7 <jafabre95@gmail.com>
2023-06-16 14:20:30 -0400

    adding train minit batch

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -4,7 +4,7 @@
 
 
 import tensorflow as tf
-import numpy as np
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32, epochs=5, load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
