ed42328c8ecdec52f0bff3a5558a59b602eaa530
joanfa7 <jafabre95@gmail.com>
2023-06-16 13:27:49 -0400

    adding new file

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def normalization_constants(X):
-    ''' calculates the normalization 
+    ''' calculates the normalization
     (standardization) constants of a matrix'''
     mean = np.mean(X, axis=0)
     std = np.std(X, axis=0)
