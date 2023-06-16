3bd11e2aeaadcc650c6ba0a78bf1ffc9be625c24
joanfa7 <jafabre95@gmail.com>
2023-06-16 14:41:44 -0400

    adding new file

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+''' updates a variable using the RMSProp optimization algorithm '''
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    ''' updates a variable using the RMSProp optimization algorithm
+        @loss: is the loss of the network
+        @alpha: is the learning rate
+        @beta2: is the RMSProp weight
+        @epsilon: is a small number to avoid division by zero
+        Returns: the RMSProp optimization operation
+    '''
+    return tf.train.RMSPropOptimizer(alpha, beta2,
+                                     epsilon=epsilon).minimize(loss)
