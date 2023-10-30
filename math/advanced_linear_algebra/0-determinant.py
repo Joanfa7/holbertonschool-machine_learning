db04ff91f47426df4f9949812da328c70232f402
Joanfa7 <jafabre95@gmail.com>
2023-10-30 12:20:31 -0400

    first commit

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,27 +1,35 @@
-#!/usr/bin/env python3
-""" Determinant """
-
-
 def determinant(matrix):
-    """ Calculates the determinant of a matrix
-        matrix: list of lists whose determinant should be calculated
-        Returns: the determinant of matrix
-    """
-    if not isinstance(matrix, list) or matrix == []:
-        raise TypeError("matrix must be a list of lists")    
-    if len(matrix) != len(matrix[0]):
+    # Check if matrix is a list of lists
+    if not (
+        isinstance(
+            matrix,
+            list) and all(
+            isinstance(
+                row,
+                list) for row in matrix)):
+        raise TypeError("matrix must be a list of lists")
+
+    # Check if matrix is square
+    rows = len(matrix)
+    if not all(len(row) == rows for row in matrix):
         raise ValueError("matrix must be a square matrix")
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        return 1
-    if len(matrix) == 1 and len(matrix[0]) == 1:
+
+    # Base case for 0x0 matrix
+    if rows == 0:
+        return 1  # The determinant of a 0x0 matrix is defined as 1
+
+    # Base case for 1x1 matrix
+    if rows == 1:
         return matrix[0][0]
-    if len(matrix) == 2 and len(matrix[0]) == 2:
-        return matrix[0][0] * matrix[1][1] - \
-            matrix[0][1] * matrix[1][0]
+
+    # Base case for 2x2 matrix
+    if rows == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    # Recursive case for larger matrices
     det = 0
-    for i, k in enumerate(matrix[0]):
-        rows = [row for row in matrix[1:]]
-        new_m = [[row[n] for n in range(len(matrix)) if n != i]
-                 for row in rows]
-        det += k * (-1) ** i * determinant(new_m)
+    for col in range(rows):
+        sub_matrix = [row[:col] + row[col + 1:]
+                      for row in (matrix[:0] + matrix[0 + 1:])]
+        det += ((-1)**col) * matrix[0][col] * determinant(sub_matrix)
     return det
