8bd2fc6f63a49cabf2ba400f119cc2858a3f1a61
Joanfa7 <jafabre95@gmail.com>
2023-11-04 20:15:07 -0400

    adding correlation

---
+++ math/multivariate_prob/2-multinormal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import numpy as np
+ 
+class MultiNormal():
+    """ Class Multinormal """
+    def __init__(self, data):
+        """ Class constructor """
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError('data must be a 2D numpy.ndarray')
+        d, n = data.shape
+        if n < 2:
+            raise ValueError('data must contain multiple data points')
+        self.mean = np.mean(data, axis=1).reshape(d, 1)
+        X = data - self.mean
+        self.cov = np.dot(X, X.T) / (n - 1)
+
