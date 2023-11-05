b487fcd9af4b5317b025f122350134367467aecd
Joanfa7 <jafabre95@gmail.com>
2023-11-04 20:17:52 -0400

    adding multinormal

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-
+""" Multinormal """""
 import numpy as np
- 
+
+
 class MultiNormal():
     """ Class Multinormal """
+
     def __init__(self, data):
         """ Class constructor """
-        if type(data) is not np.ndarray or len(data.shape) != 2:
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError('data must be a 2D numpy.ndarray')
         d, n = data.shape
         if n < 2:
@@ -14,4 +16,3 @@ class MultiNormal():
         self.mean = np.mean(data, axis=1).reshape(d, 1)
         X = data - self.mean
         self.cov = np.dot(X, X.T) / (n - 1)
-
