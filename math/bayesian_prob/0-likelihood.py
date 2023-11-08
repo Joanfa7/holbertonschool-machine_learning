4634b47666a0eae91564622060a2a87bd77c1328
Joanfa7 <jafabre95@gmail.com>
2023-11-08 11:30:39 -0400

    first commit

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" likehood """
+
+import numpy as np
+
+def likelihood(x, n, p):
+    """ """
+    if type(n) is not int or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if type(x) is not int or x < 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if type(p) is not np.ndarray:
+        raise TypeError("p must be a 1D numpy.ndarray")
+    if len(p.shape) != 1:
+        raise TypeError("p must be a 1D numpy.ndarray")
+    for i in p:
+        if i < 0 or i > 1:
+            raise ValueError("All values in p must be in the range [0, 1]")
+    fact = np.math.factorial
+    tmp = fact(n) / (fact(x) * fact(n - x))
+    return tmp * (p ** x) * ((1 - p) ** (n - x))
