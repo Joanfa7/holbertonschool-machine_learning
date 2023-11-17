5fefcde3013f5e4f6f0b5699ac56c67f06ecfd4f
Joanfa7 <jafabre95@gmail.com>
2023-11-17 11:45:54 -0400

    fix pycodestyle

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -2,27 +2,31 @@
 """ calculates the probability density function of a Gaussian distribution"""
 import numpy as np
 
+
 def pdf(X, m, S):
-    """ calculates the probability density function of a Gaussian distribution"""
-    if type(X) != np.ndarray or len(X.shape) != 2:
+    """ calculates the probability density function 
+    of a Gaussian distribution"""
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
-    if type(m) != np.ndarray or len(m.shape) != 1:
+    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
         return None
-    if type(S) != np.ndarray or len(S.shape) != 2:
+    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
         return None
     n, d = X.shape
     n, d = X.shape
 
     if m.shape[0] != d or S.shape[0] != d or S.shape[1] != d:
-        # Checks if the dimensions of m and S are compatible with the dimensions of X.
+        # Checks if the dimensions of m and S are compatible with the
+        # dimensions of X.
         return None
 
     det = np.linalg.det(S)
     inv = np.linalg.inv(S)
     den = np.sqrt((2 * np.pi) ** d * det)
 
-    # Calculates the squared Mahalanobis distance between each data point and the mean vector.
-    fac = np.einsum('...k,kl,...l->...', X-m, inv, X-m)
+    # Calculates the squared Mahalanobis distance between each data point and
+    # the mean vector.
+    fac = np.einsum('...k,kl,...l->...', X - m, inv, X - m)
 
     # Calculates the probability density function  each data point.
     pdf = np.exp(-fac / 2) / den
