393fd0d3f941d82a312f294bebff1907cdd716d8
Joanfa7 <jafabre95@gmail.com>
2023-11-18 18:35:27 -0400

    adding kemas method using sklearn

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" K-means """
+
+import sklearn.cluster
+
+def kmeans(X, k):
+    """ performs K-means on a dataset
+    Arg:
+        - X: np.ndarray shape(n, d) dataset
+            - n: number of data points
+            - d: number of dimensions
+        - k: positive int number of clusters
+    Returns: C, clss
+        - C: np.ndarray shape(k, d) centroid means for each cluster
+        - clss: np.ndarray shape(n,) index of cluster in C that each data point belongs to
+    """
+    kmeans = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+    return kmeans.cluster_centers_, kmeans.labels_
