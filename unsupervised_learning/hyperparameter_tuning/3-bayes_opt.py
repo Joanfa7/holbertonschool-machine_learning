bee3b10984b705cb578ca17e056f3d6d4cfc3d7f
Joanfa7 <jafabre95@gmail.com>
2023-11-30 14:14:12 -0400

    fix bug

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -7,10 +7,22 @@ GP = __import__('2-gp').GaussianProcess
 
 class BayesianOptimization:
     """ Performs Bayesian Optimization on a noiseless 1D Gaussian process """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+
+    def __init__(
+            self,
+            f,
+            X_init,
+            Y_init,
+            bounds,
+            ac_samples,
+            l=1,
+            sigma_f=1,
+            xsi=0.01,
+            minimize=True):
         """ Class constructor """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
+        X_s = np.linspace(bounds[0], bounds[1], num=ac_samples)
+        self.X_s = np.reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
