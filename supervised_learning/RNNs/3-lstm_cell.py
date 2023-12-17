13c8529ffddd1d0cd97f86ec5906d9639bca6443
Joanfa7 <jafabre95@gmail.com>
2023-12-17 17:25:37 -0400

    adding sigmoid and softmax functions to GruCell class

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -18,38 +18,37 @@ class LSTMCell:
         self.Wu = np.random.normal(size=(i+h, h))
         self.Wc = np.random.normal(size=(i+h, h))
         self.Wo = np.random.normal(size=(i+h, h))
-        self.Wy = np.random.normal(size=(i+h, h))
+        self.Wy = np.random.normal(size=(h, o))
         self.bf = np.zeros((1, h))
         self.bu = np.zeros((1, h))
         self.bc = np.zeros((1, h))
         self.bo = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
-        def forward(self, h_prev, c_prev, x_t):
-            """ perform forward propagation """
-            # Concat h_prev and x_t to match Wh dimensions
-            x = np.concatenate((h_prev.T, x_t.T), axis=0)
-            # Forget gate
-            f = self.sigmoid(np.matmul(x.T, self.Wf) + self.bf)
-            # Update gate
-            u = self.sigmoid(np.matmul(x.T, self.Wu) + self.bu)
-            # Candidate
-            c = np.tanh(np.matmul(x.T, self.Wc) + self.bc)
-            # Cell state
-            c_next = f * c_prev + u * c
-            # Output gate
-            o = self.sigmoid(np.matmul(x.T, self.Wo) + self.bo)
-            # Hidden state
-            h_next = o * np.tanh(c_next)
-            # Softmax activation function
-            y = np.matmul(h_next, self.Wy) + self.by
-            y = np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
-            return h_next, c_next, y
-        
-        def sigmoid(self, x):
-            """ Sigmoid function """
-            return 1 / (1 + np.exp(-x))
-        
-        def softmax(self, x):
-            """ Softmax function """
-            return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+    def forward(self, h_prev, c_prev, x_t):
+        """ perform forward propagation """
+        # Concat h_prev and x_t to match Wh dimensions
+        x = np.concatenate((h_prev, x_t), axis=1)
+        # Forget gate
+        f = self.sigmoid(np.matmul(x, self.Wf) + self.bf)
+        # Update gate
+        u = self.sigmoid(np.matmul(x, self.Wu) + self.bu)
+        # Candidate
+        c = np.tanh(np.matmul(x, self.Wc) + self.bc)
+        # Cell state
+        c_next = f * c_prev + u * c
+        # Output gate
+        o = self.sigmoid(np.matmul(x, self.Wo) + self.bo)
+        # Hidden state
+        h_next = o * np.tanh(c_next)
+        # Softmax activation function
+        y = self.softmax(np.matmul(h_next, self.Wy) + self.by)
+        return h_next, c_next, y
+
+    def sigmoid(self, x):
+        """ Sigmoid function """
+        return 1 / (1 + np.exp(-x))
+
+    def softmax(self, x):
+        """ Softmax function """
+        return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
