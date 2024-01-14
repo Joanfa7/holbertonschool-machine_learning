8560d788996fb2a94f2389400ddf6508e29ef235
Joanfa7 <jafabre95@gmail.com>
2024-01-14 16:10:11 -0400

    pycodestyle

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
+
 class Encoder(tf.keras.layers.Layer):
     """ class encoder """
     def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
