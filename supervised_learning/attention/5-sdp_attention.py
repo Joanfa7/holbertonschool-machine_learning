2acb5e08c5d5f25236830f841f0c41541fb579c5
Joanfa7 <jafabre95@gmail.com>
2024-01-14 13:59:36 -0400

    adding sdp_attention method

---
+++ supervised_learning/attention/5-sdp_attention.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+SelfAttention = __import__('1-self_attention').SelfAttention
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """ sdp_attention - calculates the scaled dot product attention """
+    Q = tf.matmul(Q, K, transpose_b=True)
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled = Q / tf.math.sqrt(dk)
+    if mask is not None:
+        scaled += (mask * -1e9)
+    weights = tf.nn.softmax(scaled, axis=-1)
+    output = tf.matmul(weights, V)
+    return output, weights
