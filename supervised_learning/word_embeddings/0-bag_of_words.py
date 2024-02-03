c0dc6435aa2b22327b550ebd61e26aff7589d16f
Joanfa7 <jafabre95@gmail.com>
2024-02-03 17:33:40 -0400

    fix bug

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -7,8 +7,8 @@ def bag_of_words(sentences, vocab=None):
     ''' creates a bag of words embedding matrix '''
     vectorizer = CountVectorizer(max_features=sentences)
     X = vectorizer.fit_transform(sentences)
-    if vocabulary is None:
-        vocabulary = []
+    if vocab is None:
+        vocab = []
     embeddings = vectorizer.fit_transform(sentences).toarray()
     vocab = list(vectorizer.get_feature_names())
     return embeddings, vocab
